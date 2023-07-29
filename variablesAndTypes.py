c6b00881a1dddc641710dbc0de21e85dd09796df
actuallymrtn <misanjurjoisla@gmail.com>
2023-07-29 15:01:16 -0300

    2-Default Project Files With a Plus
    its a Variables and Types practice script

---
+++ variablesAndTypes.py
@@ -0,0 +1,29 @@
+#Variable and Types
+
+intNumber = 7 # Integer Variable
+print(intNumber)
+
+floatNumber = float(7.9) # Float Variable
+print(floatNumber)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+
+print(helloworld)
+
+print(hello, "it's me mario")
+
+a, b = 3, 4
+print(a, b)
+
+# Mixing Operators Between Numbers and Strings is Not Supported
+
+mystring = "Hello"
+print(mystring)
+
+myfloat = float(10.0) # cheated looked documentation
+print(myfloat)
+
+myint = 20
+print(myint)
