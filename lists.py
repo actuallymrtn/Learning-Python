686bc64a63f9eaf7fa24cce0e91c2a6ba4c8a1f8
actuallymrtn <misanjurjoisla@gmail.com>
2023-07-29 16:20:31 -0300

    I did this script the right way

---
+++ lists.py
@@ -0,0 +1,26 @@
+# Lists
+
+myList = []
+myList.append(1)
+myList.append(2)
+myList.append(3)
+print(myList[0]) # prints 1
+print(myList[1]) # prints 2
+print(myList[2]) # prints 3
+
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+
+second_name = names[1]
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append("hello")
+strings.append("world")
+
+print(numbers)
+print(strings)
+print("The Second Name on The Names List is %s" % second_name)
